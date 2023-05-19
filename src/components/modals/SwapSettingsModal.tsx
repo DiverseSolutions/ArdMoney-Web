7561d30a44e964d935a7e2ac70c2590a477a212b
mnkhod <mnkhod.dev@gmail.com>
2023-05-19 15:48:44 +0800

    Added slippage,deadline to dexSlice & configured it on SwapSettingsModal

--- src/components/modals/SwapSettingsModal.tsx
+++ src/components/modals/SwapSettingsModal.tsx
@@ -1,14 +1,45 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { setSwapSettingsModal } from "@slices/modalSlice";
 
 import CloseIcon from "@assets/icons/CloseIcon";
 import ModalLayout from "../layouts/ModalLayout";
+import { FormEvent, useState } from "react";
+import { RootState } from "@/redux/store";
+import { isEmpty } from "radash";
+import { setDeadline, setSlippage } from "@slices/dexSlice";
 
 export default function SwapSettingsModal() {
+  const { slippage,deadline } = useSelector((state:RootState) => state.dex)
+  const [localSlippage,setLocalSlippage] = useState(slippage)
+  const [localDeadline,setLocalDeadline] = useState(deadline)
   const dispatch = useDispatch();
 
   function handleModalClose() {
     dispatch(setSwapSettingsModal(false));
+  }
+
+  function handleSlippageChange(e: FormEvent<HTMLInputElement>){
+    const inputValue = e.currentTarget.value;
+    if(isEmpty(inputValue)){
+      dispatch(setSlippage(0))
+      setLocalSlippage(0)
+    }
+
+    let amount = parseFloat(inputValue)
+    dispatch(setSlippage(amount))
+    setLocalSlippage(amount)
+  }
+
+  function handleDeadlineChange(e: FormEvent<HTMLInputElement>){
+    const inputValue = e.currentTarget.value;
+    if(isEmpty(inputValue)){
+      dispatch(setDeadline(0))
+      setLocalDeadline(0)
+    }
+
+    let amount = parseFloat(inputValue)
+    dispatch(setDeadline(amount))
+    setLocalDeadline(amount)
   }
 
   return (
@@ -27,14 +58,14 @@ export default function SwapSettingsModal() {
         <span className="text-light-terteriary">Slippage Tolerance</span>
 
         <div className="flex gap-4xs justify-between items-center w-full border border-primary/20 rounded-lg text-white p-sm mb-sm">
-          <input type="number" className="text-right bg-transparent w-full focus-visible:outline-none" value={0.5} />
+          <input type="number" className="text-right bg-transparent w-full focus-visible:outline-none" value={localSlippage} onChange={handleSlippageChange} />
           <span className="relative top-0.5 text-xs">%</span>
         </div>
 
         <span className="text-light-terteriary">Transaction Deadline</span>
 
         <div className="flex gap-4xs justify-between items-center w-full border border-primary/20 rounded-lg text-white p-sm mb-sm">
-          <input type="number" className="bg-transparent text-right w-full focus-visible:outline-none" value={30} />
+          <input type="number" className="bg-transparent text-right w-full focus-visible:outline-none" value={localDeadline} onChange={handleDeadlineChange} />
           <span className="relative top-0.5 text-xs">min</span>
         </div>
       </div>
--- src/redux/slices/dexSlice.tsx
+++ src/redux/slices/dexSlice.tsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 interface SubgraphIndexType<TValue> {
     [id: number]: TValue;
@@ -6,6 +6,8 @@ interface SubgraphIndexType<TValue> {
 
 export interface DexState {
   subgraph : SubgraphIndexType<string>;
+  slippage: number,
+  deadline: number,
 }
 
 const dexSlice = createSlice({
@@ -15,9 +17,20 @@ const dexSlice = createSlice({
       56: "https://api.thegraph.com/subgraphs/name/mnkhod/ardmoney-bnb-v1-subgraph",
       97: "https://api.thegraph.com/subgraphs/name/mnkhod/ardmoney-chapel-v4-subgraph",
     },
+    slippage: 0.5,
+    deadline: 30,
   } as DexState,
-  reducers: {},
+  reducers: {
+    setSlippage: (state, action: PayloadAction<number>) => {
+      state.slippage = action.payload;
+    },
+    setDeadline: (state, action: PayloadAction<number>) => {
+      state.deadline = action.payload;
+    },
+  },
 })
+
+export const { setSlippage,setDeadline } = dexSlice.actions;
 
 export const dexReducer = dexSlice.reducer
 
