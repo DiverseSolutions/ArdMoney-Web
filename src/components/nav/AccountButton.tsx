c14b17899255d3db67e0338e1f9db2a1014f22b3
mnkhod <mnkhod.dev@gmail.com>
2023-06-17 13:48:08 +0800

    Modified Wallet Button

--- src/components/nav/AccountButton.tsx
+++ src/components/nav/AccountButton.tsx
@@ -2,9 +2,10 @@ import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "@redux/store";
 import OutlineButton from "@components/shared/OutlineButton";
 import { setWalletModal } from "@slices/modalSlice";
+import { useTranslation } from "react-i18next";
 
 export default function AccountButton() {
-  const { account } = useSelector((state: RootState) => state.web3);
+  const { t } = useTranslation();
   const dispatch = useDispatch();
 
   return (
@@ -15,16 +16,7 @@ export default function AccountButton() {
       }}
     >
       <div className="i-ic-outline-account-balance-wallet icon-size-5" />
-      {accountNameShortener(account)}
+      {t("navBar:wallet")}
     </OutlineButton>
   );
-}
-
-function accountNameShortener(account: string | undefined) {
-  if (!account) return "";
-
-  return `${account.substring(0, 6)}...${account.substring(
-    account.length - 5,
-    account.length
-  )}`;
 }
