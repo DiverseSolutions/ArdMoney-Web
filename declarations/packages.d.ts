020aa912eb756309f24787ffc6fcdbe40a6e62f3
mnkhod <mnkhod.dev@gmail.com>
2023-06-21 16:30:50 +0800

    Removed use-mobile-detect-hook , Added useScreenType

--- declarations/packages.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "use-mobile-detect-hook" {
-  declare const useMobileDetect: () => {
-    isMobile: () => boolean;
-    isDesktop: () => boolean;
-    isAndroid: () => boolean;
-    isIos: () => boolean;
-    isSSR: () => boolean;
-  };
-}
--- src/components/modals/NetworkModal.tsx
+++ src/components/modals/NetworkModal.tsx
@@ -8,11 +8,12 @@ import { setNetworkConfigured, setNetworkUnknown } from "@slices/networkSlice";
 import ModalLayout from "../layouts/ModalLayout";
 import Divider from "@components/shared/Divider";
 import { useTranslation } from "react-i18next";
-import { useMobileDetect } from "use-mobile-detect-hook";
+import useScreenType from "react-screentype-hook";
 
 export default function NetworkModal() {
   const { t } = useTranslation();
-  const detectMobile = useMobileDetect();
+  const { isMobile } = useScreenType();
+
   const dispatch = useDispatch();
 
   function handleModalClose() {
@@ -38,9 +39,10 @@ export default function NetworkModal() {
       }
     }
   }
+
   return (
     <ModalLayout
-      alignment={detectMobile.isMobile() ? "center" : "right"}
+      alignment={isMobile ? "center" : "right"}
       handleModalClose={handleModalClose}
     >
       <div className="flex flex-col gap-xl w-full">
