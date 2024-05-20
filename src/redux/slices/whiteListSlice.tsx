8c89e3aea07f6cc5629139c27bc1b53819f781cb
Munkh-Od <mnkhod.dev@gmail.com>
2024-05-20 15:02:12 +0800

    Whitelisted MONT/IHC

--- src/redux/slices/whiteListSlice.tsx
+++ src/redux/slices/whiteListSlice.tsx
@@ -12,6 +12,8 @@ const whiteListSlice = createSlice({
       "0x6818d4ae9bcec60cbef6daf26ebfafc12d281aae",
       // MONT/ARDM
       "0xcf013f607e6a5bf0ae7860ffe46a23ce1a262839",
+      // MONT/IHC
+      "0xdf36b46abcf4acf70553ba8f84c93b52e4227dfa",
     ],
   } as WhiteListState,
   reducers: {},
